02e85e93645eff941312b86c1b2f77f7f8f77802
Leonardo Barnabe <barnadsv@gmail.com>
2017-08-25 22:50:20 -0300

    Commit inicial

--- GestureApp/ViewController.swift
+++ GestureApp/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapDetected(_ sender: UITapGestureRecognizer) {
+        statusLabel.text = "Tap detectado"
+    }
+    
+    @IBAction func pinchDetected(_ sender: UIPinchGestureRecognizer) {
+        let scale = sender.scale
+        let velocity = sender.velocity
+        let resultString = "Pinch detectado \nEscala = \(String(format: "%.02f", scale)) \nVelocidade = \(String(format: "%.02f", velocity))"
+        statusLabel.text = resultString
+    }
+    
+    @IBAction func swipeDetected(_ sender: UISwipeGestureRecognizer) {
+        statusLabel.text = "Swipe à direita detectado"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
